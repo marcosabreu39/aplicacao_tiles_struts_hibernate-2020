44173e3494de60dcd09f12e2d2daa24ddbaac3e3
marco <marco@LAPTOP-HITOHP1E>
2020-05-02 15:40:25 -0300

    Foi realizada a integração entre os Designs Patterns Facade e repository.

---
+++ src/main/java/com/crud/facade/UsuarioFacadeImpl.java
@@ -0,0 +1,64 @@
+package com.crud.facade;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import com.crud.model.Usuario;
+import com.crud.repository.UsuarioRepository;
+
+public class UsuarioFacadeImpl implements UsuarioFacade {
+	
+	@Inject
+	UsuarioRepository usuarioRepository;
+
+	@Override
+	public Usuario busca(Integer id) throws Exception {
+		
+		return usuarioRepository.busca(id);
+	}
+
+
+	@Override
+	public void adiciona(Usuario usuario) throws Exception {
+		
+		usuarioRepository.adiciona(usuario);
+	}
+
+
+	@Override
+	public void remove(Integer id) throws Exception {
+		
+		usuarioRepository.remove(id);
+
+	}
+
+
+	@Override
+	public void altera(Usuario usuario) throws Exception {
+		
+		usuarioRepository.altera(usuario);
+	}
+
+
+	@Override
+	public List<Usuario> lista() throws Exception {
+		
+		return usuarioRepository.lista();
+	}
+
+
+	@Override
+	public boolean existeUsernameEPassword(String userName, String password) throws Exception {
+		
+		return usuarioRepository.existeUsernameEPassword(userName, password);
+	}
+
+
+	@Override
+	public boolean existeUsername(String userName) throws Exception {
+		
+		return usuarioRepository.existeUsername(userName);
+	}
+
+}
